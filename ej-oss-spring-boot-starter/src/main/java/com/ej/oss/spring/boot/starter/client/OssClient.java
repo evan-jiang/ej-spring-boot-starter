90ef42560d354108646850c89aa0113b69ee5849
jiangyongsheng <jiangyongsheng@zhongan.com>
2019-08-14 14:17:29 +0800

    youhua

--- ej-oss-spring-boot-starter/src/main/java/com/ej/oss/spring/boot/starter/client/OssClient.java
+++ ej-oss-spring-boot-starter/src/main/java/com/ej/oss/spring/boot/starter/client/OssClient.java
@@ -1,19 +1,17 @@
 package com.ej.oss.spring.boot.starter.client;
 
-import java.io.File;
-
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.OSSClientBuilder;
 import com.aliyun.oss.model.GetObjectRequest;
 import com.ej.oss.spring.boot.starter.exception.OssFileNotFoundException;
 import com.ej.oss.spring.boot.starter.properties.OssConfig;
 
+import java.io.File;
+
 /**
- * 
- * @Description: OSS客户端
  * @author Evan Jiang
+ * @Description: OSS客户端
  * @date 2019年4月22日 下午6:45:14
- *
  */
 public class OssClient {
 
@@ -27,10 +25,9 @@ public class OssClient {
 
     /**
      * 初始化oss客户端
-     * 
+     *
      * @param ossConfig
-     * @param isMulti
-     *            是否属多连接，判断是否需要校验连接的别名，用于工厂管理
+     * @param isMulti   是否属多连接，判断是否需要校验连接的别名，用于工厂管理
      */
     public OssClient(OssConfig ossConfig, boolean isMulti) {
         if (isMulti) {
@@ -42,7 +39,6 @@ public class OssClient {
     }
 
     /**
-     * 
      * @Description: 创建oss连接
      * @author Evan Jiang
      * @date 2019年4月23日 下午5:06:16
@@ -54,7 +50,6 @@ public class OssClient {
     }
 
     /**
-     * 
      * @Description: 关闭oss连接
      * @author Evan Jiang
      * @date 2019年4月23日 下午5:06:47
@@ -66,7 +61,6 @@ public class OssClient {
     }
 
     /**
-     * 
      * @Description: 获取可用的oss连接
      * @author Evan Jiang
      * @date 2019年4月23日 下午5:05:36
@@ -81,31 +75,14 @@ public class OssClient {
     }
 
     /**
-     * 
-     * @Description: 判断oss上指定文件是否存在
-     * @author Evan Jiang
-     * @date 2019年4月23日 下午5:07:20
-     * @param key
-     * @return
-     */
-    public boolean exists(String key) {
-        return usableOssClient().doesObjectExist(ossConfig.getBucketName(),
-                key);
-    }
-
-    /**
-     * 
-     * @Description: 下载文件
-     * @author Evan Jiang
-     * @date 2019年4月23日 下午5:07:48
      * @param key
      * @param path
      * @return
+     * @Description: 下载文件
+     * @author Evan Jiang
+     * @date 2019年4月23日 下午5:07:48
      */
     public boolean downloadFile(String key, String path) {
-        if (!exists(key)) {
-            throw new OssFileNotFoundException("远程文件[" + key + "]不存在");
-        }
         File file = new File(path);
         File parentFile = file.getParentFile();
         if (!parentFile.exists()) {
@@ -118,13 +95,12 @@ public class OssClient {
     }
 
     /**
-     * 
-     * @Description: 上传文件
-     * @author Evan Jiang
-     * @date 2019年4月23日 下午5:08:08
      * @param key
      * @param path
      * @return
+     * @Description: 上传文件
+     * @author Evan Jiang
+     * @date 2019年4月23日 下午5:08:08
      */
     public boolean uploadFile(String key, String path) {
         File file = new File(path);
